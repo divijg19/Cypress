5e2f52c9e74b109a8b1b5a2fa2918e216be1dd41
divijg19 <divijganjoo2003@gmail.com>
2026-04-01 14:26:25 +0530

    Nvim: star loops

--- nvim/inv-stars.c
+++ nvim/inv-stars.c
@@ -2,9 +2,9 @@
 
 int main(void) {
   int n = 0;
-  printf("How tall are the starry stairs? \n");
+  printf("How tall are the inverted stairs? \n");
   scanf("%d", &n);
-  for (int i = 1; i <= n; i++) {
+  for (int i = n; i >= 1; i--) {
     for (int j = 1; j <= i; j++) {
       printf("*");
     }
